6e238d048047131a65f8db2da6ef26010fcf1b4f
coglenn <codagl@@msn.com>
2023-09-13 21:04:22 -0600

    db config

--- config.py
+++ config.py
@@ -13,13 +13,13 @@ class Config:
     db = SQLAlchemy()
     admin = Admin()
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    db_type = os.getenv("DB_TYPE", "mysql")
-    user = os.getenv("DB_USER", "root")
-    passwd = os.getenv("DB_PASSWD", "Nelly3253")
-    host = os.getenv("DB_HOST", "127.0.0.1")
-    port = os.getenv("DB_PORT", 3306)
-    db_name = os.getenv("DB_NAME", "flaskshop")
-    SQLALCHEMY_DATABASE_URI = os.environ.get(f"mysql+pymysql://{user}:{passwd}@{host}:{port}/{db_name}?charset=utf8mb4")\
+    db_type = os.getenv("DB_TYPE", "postgresql")
+    user = os.getenv("DB_USER", "DB_USER")
+    passwd = os.getenv("DB_PASSWD", "DB_PASSWD")
+    host = os.getenv("DB_HOST", "localhost")
+    port = os.getenv("DB_PORT", 5432)
+    db_name = os.getenv("DB_NAME", "thebigda_tbd")
+    SQLALCHEMY_DATABASE_URI = os.environ.get(f"postgresql://{user}:{passwd}@{host}:{port}/{db_name}")\
         or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
