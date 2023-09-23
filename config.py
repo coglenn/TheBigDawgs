29f1cb62f976bd283b893709144e1c2d7f219dea
coglenn <codagl@@msn.com>
2023-09-22 20:57:05 -0600

    db

--- config.py
+++ config.py
@@ -9,17 +9,25 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 UPLOAD_FOLDER = 'static/uploads/'
 
-class Config:
-    db = SQLAlchemy()
-    admin = Admin()
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+class DBConfig:
     db_type = os.getenv("DB_TYPE", "postgresql")
     user = os.getenv("DB_USER", "DB_USER")
     passwd = os.getenv("DB_PASSWD", "DB_PASSWD")
     host = os.getenv("DB_HOST", "localhost")
     port = os.getenv("DB_PORT", 5432)
     db_name = os.getenv("DB_NAME", "thebigda_tbd")
-    SQLALCHEMY_DATABASE_URI = os.environ.get(f"postgresql://{user}:{passwd}@{host}:{port}/{db_name}")
+    db_uri = f"postgresql://{user}:{passwd}@{host}:{port}/{db_name}"
+
+class Config:
+    db = SQLAlchemy()
+    admin = Admin()
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+    SQLALCHEMY_DATABASE_URI = os.getenv("DB_URI", DBConfig.db_uri)
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    DATABASE_QUERY_TIMEOUT = 0.1  # log the slow database query, and unit is second
+    SQLALCHEMY_RECORD_QUERIES = True
+    # SQLALCHEMY_DATABASE_URI = os.environ.get(f"postgresql://{user}:{passwd}@{host}:{port}/{db_name}")\
         # or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
